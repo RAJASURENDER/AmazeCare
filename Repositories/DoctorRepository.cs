b83acd9d642207256ba6ac1883ed9a7a82246e2a
Aditi Thakur <aditi9653@gmail.com>
2024-02-12 21:40:26 +0530

    Updated Repository

--- Repositories/DoctorRepository.cs
+++ Repositories/DoctorRepository.cs
@@ -12,12 +12,25 @@ namespace AmazeCare.Repositories
      
         RequestTrackerContext _context;
         ILogger<DoctorRepository> _logger;
+
+        /// <summary>
+        /// Initializes a new instance of the DoctorRepository class with the specified context and logger.
+        /// </summary>
+        /// <param name="context">The database context.</param>
+        /// <param name="logger">The logger instance.</param>
+
         public DoctorRepository(RequestTrackerContext context, ILogger<DoctorRepository>
        logger)
         {
             _context = context;
             _logger = logger;
         }
+
+        /// <summary>
+        /// Adds a new doctor to the database.
+        /// </summary>
+        /// <param name="item">The doctor to add.</param>
+        /// <returns>The added doctor.</returns>
 
         public async Task<Doctors> Add(Doctors item)
         {
@@ -27,6 +40,13 @@ namespace AmazeCare.Repositories
             return item;
         }
 
+
+        /// <summary>
+        /// Deletes a doctor from the database based on the specified key.
+        /// </summary>
+        /// <param name="key">The key of the doctor to delete.</param>
+        /// <returns>The deleted doctor.</returns>
+
         public async Task<Doctors> Delete(int key)
         {
             var doctor = await GetAsync(key);
@@ -35,6 +55,13 @@ namespace AmazeCare.Repositories
             _logger.LogInformation("Doctor deleted " + key);
             return doctor;
         }
+
+        /// <summary>
+        /// Retrieves a doctor from the database based on the specified key.
+        /// </summary>
+        /// <param name="key">The key of the doctor to retrieve.</param>
+        /// <returns>The retrieved doctor.</returns>
+        /// <exception cref="NoSuchDoctorException">Thrown when the doctor with the specified key does not exist.</exception>
 
         public async Task<Doctors> GetAsync(int key)
         {
@@ -47,11 +74,22 @@ namespace AmazeCare.Repositories
             throw new NoSuchDoctorException();
         }
 
+        /// <summary>
+        /// Retrieves all doctors from the database.
+        /// </summary>
+        /// <returns>A list of all doctors.</returns>
+
         public async Task<List<Doctors>> GetAsync()
         {
             var doctors = _context.Doctors.ToList();
             return doctors;
         }
+
+        /// <summary>
+        /// Updates the information of a doctor in the database.
+        /// </summary>
+        /// <param name="item">The doctor object with updated information.</param>
+        /// <returns>The updated doctor.</returns>
 
         public async Task<Doctors> Update(Doctors item)
         {
